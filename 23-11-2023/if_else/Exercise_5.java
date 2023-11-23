d7b646a757ec9792f1ab83b4467afbdb587f7d02
Victor Henrique <41922049+Squivoix@users.noreply.github.com>
2023-11-23 16:29:04 -0300

    Add files via upload

--- 23-11-2023/if_else/Exercise_5.java
+++ 23-11-2023/if_else/Exercise_5.java
@@ -5,20 +5,16 @@ import java.util.Scanner;
 public class Exercise_5 {
 
 	public static void main(String[] args) {
-		int width, height, length, area, max = 50;
+		int height, max = 50;
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Digite a largura do pacote: "); width = scanner.nextInt();
 		System.out.print("Digite a altura do pacote: "); height = scanner.nextInt();
-		System.out.print("Digite o comprimento do pacote: "); length = scanner.nextInt();
 		
-		area = width * height * length;
-		
-		if(area > max) {
-			System.out.println(String.format("A área do pacote é maior que o máximo! %s maior que %s", area, max));
+		if(height > max) {
+			System.out.println(String.format("A altura do pacote é maior que o máximo! %s maior que %s", height, max));
 		} else {
-			System.out.println(String.format("A área do pacote é menor que o máximo! %s menor que %s", area, max));
+			System.out.println(String.format("A altura do pacote é menor que o máximo! %s menor que %s", height, max));
 		}
 		
 		scanner.close();
